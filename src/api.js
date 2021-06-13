b430a144d115d15059ae10081e3de2370022a42e
nandhu-ar <nandhuaregi1996@gmail.com>
2021-06-13 19:03:25 +0530

    api changed to heroku

--- src/api.js
+++ src/api.js
@@ -5,29 +5,29 @@ import axios from 'axios';
 
 
 export const validateLogin = async(user) => {
-    return axios.post("http://localhost:8080/user", user);
+    return axios.post("https://employee-api-service.herokuapp.com/user", user);
 }
 
 export const addUser = async(user) => {
-    return axios.put("http://localhost:8080/user", user);
+    return axios.put("https://employee-api-service.herokuapp.com/user", user);
 }
 
 export const getAllEmployees = async() => {
-    return axios.get("http://localhost:8080/employee",config);
+    return axios.get("https://employee-api-service.herokuapp.com/employee",config);
 }
 
 export const registerEmployee = async(employee) => {
-    return axios.put("http://localhost:8080/employee", employee, config);
+    return axios.put("https://employee-api-service.herokuapp.com/employee", employee, config);
 }
 
 export const updateEmployee = async(employee) => {
-    return axios.patch("http://localhost:8080/employee", employee, config);
+    return axios.patch("https://employee-api-service.herokuapp.com/employee", employee, config);
 }
 
 export const deleteEmployee = async(employeeId) => {
-    return axios.delete(`http://localhost:8080/employee/${employeeId}`, config);
+    return axios.delete(`https://employee-api-service.herokuapp.com/employee/${employeeId}`, config);
 }
 
 export const filterEmployee = async(filters) => {
-    return axios.post("http://localhost:8080/employee", filters, config);
+    return axios.post("https://employee-api-service.herokuapp.com/employee", filters, config);
 }
